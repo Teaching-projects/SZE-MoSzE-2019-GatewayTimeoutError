fb1ba714038c03078ff831f2a924bf100c45839f
szerdahelyir <47789479+szerdahelyir@users.noreply.github.com>
2019-10-15 22:18:05 +0200

    absolutepath
    absolutepath kezdetleges

--- filesystem.cpp
+++ filesystem.cpp
@@ -24,24 +24,41 @@ int filesystem::mkdir(string n)
     currentdir->makefolder(n);
     return 1;
 }
-
+int filesystem::touch(string filename){
+    for(auto i:currentdir->getSubdirectories()){
+        if(i->getName()==filename){
+            return 0;
+        }
+    }
+    for(auto i:currentdir->getFiles()){
+        if(i->getFilename()==filename){
+            return 0;
+        }
+    }
+    currentdir->touch(filename);
+    return 1;
+}
 void filesystem::ls(){
     directory* tmp;
     tmp=currentdir;
-    list<string> path;
+    path.push_front(currentdir->getName());
     while(tmp->getParent()!=nullptr){
         path.push_front(tmp->getParent()->getName());
         tmp=tmp->getParent();
     }
+    //currentdir->print();
+    //cout<<"/"<<endl;
+    for(auto i:path){
+        cout<<i;
+        cout<<"/";
+    }
+    cout<<endl;
     for(auto i:currentdir->getSubdirectories()){
-        for(auto i:path){
-            cout<<i;
-            cout<<"/";
-        }
-        currentdir->print();
         i->ls();
         cout<<endl;
     }
+    currentdir->lsfiles();
+    path.clear();
 }
 //cd mukodik
 int filesystem::cd(string to){
@@ -49,14 +66,56 @@ int filesystem::cd(string to){
         currentdir=currentdir->getParent();
         return 1;
     }
+    int tmp=0;
     for(auto& i:currentdir->getSubdirectories()){
         //i->print();
         if(i->getName()==to){
+        tmp+=1;
         currentdir=i;
         return 1;
         }
     }
+    if(tmp==0){
+        cout<<"Az adott mappa nem letezik"<<endl;
+    }
     return 0;
+}
+
+vector<string> filesystem::split(const string& str, const char& delim)
+{
+    vector<string> tokens;
+    size_t prev = 0, pos = 0;
+    do
+    {
+        pos = str.find(delim, prev);
+        if (pos == string::npos) pos = str.length();
+        string token = str.substr(prev, pos-prev);
+        if (!token.empty()) tokens.push_back(token);
+        prev = pos + 1;
+    }
+    while (pos < str.length() && prev < str.length());
+    return tokens;
+}
+
+int filesystem::absoulutepath(string path){
+    currentdir=root;
+    vector<string>route=this->split(path,'/');
+    int flag=1;
+    for(unsigned int i=1;i<route.size();i++){
+        for(auto j:currentdir->getSubdirectories()){
+            if(j->getName()==route[i]){
+                currentdir=j;
+                flag++;
+            }
+        }
+    }
+    if(flag==route.size()){
+        return 1;
+    }
+    else{
+        cout<<"Nincs ilyen eleresi utvonal"<<endl;
+        return 0;
+    }
 }
 
 
@@ -79,6 +138,9 @@ void filesystem::start(){
            else if (command=="cd")
            {
                if(argument1=="") cout<<"Adjon meg egy mappanevet"<<endl;
+               else if(argument1.find("/")!=string::npos){
+                   this->absoulutepath(argument1);
+               }
                else{
                    this->cd(argument1);
                }
@@ -113,8 +175,14 @@ void filesystem::start(){
                    currentdir->rmrf(argument1);
                }
            }
+           else if(command=="touch"){
+               if(argument1=="")
+                   cout<<"Adja meg a fajl nevet amit letre akar hozni"<<endl;
+               else {
+                   this->touch(argument1);
+               }
+           }
            else std::cout<<"Unknown command"<<std::endl;
     } while (command!="quit");
 }
-
 
--- filesystem.h
+++ filesystem.h
@@ -3,20 +3,25 @@
 
 #include "directory.h"
 #include <list>
+#include <vector>
 
 class filesystem
 {
 private:
     list <directory*> directories;
     directory* root;
+    list<string> path;
 public:
     filesystem();
     ~filesystem();
     int mkdir(string);
+    int touch(string);
     void ls();
     int cd(string);
     directory* currentdir;
     void start();
+    vector<string> split(const string&, const char&);
+    int absoulutepath(string);
 };
 
 #endif // FILESYSTEM_H
